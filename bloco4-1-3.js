502c6b0591eb28a6a6b9b6990c6095705c980e98
Julio Cesar <juliosinu@gmail.com>
2021-05-19 20:09:06 -0300

    bloco 4.1 exercicio 3 finished

---
+++ bloco4-1-3.js
@@ -0,0 +1,21 @@
+let a = 9;
+let b = 10;
+let c = 10;
+if (a>b){ 
+  if (a>c){
+    console.log(a);
+  }  
+}
+else if (b>a){ 
+  if (b>c){
+    console.log(b);
+  }
+}
+else if (c>a){
+  if (c>b){
+    console.log(c);
+  }
+}
+else {
+  console.log ('nuemros iguais');
+}
