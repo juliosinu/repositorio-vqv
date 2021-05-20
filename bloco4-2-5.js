534ea779979a852025a27192acdda3a73b34a2bd
Julio Cesar <juliosinu@gmail.com>
2021-05-20 18:52:29 -0300

    exercise 6 block 4 finished

--- bloco4-2-5.js
+++ bloco4-2-5.js
@@ -1,9 +1,9 @@
-let numbers = [5, 9, 3, 19, 70, 8, 2, 35, 27];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 contador = numbers[0];
 numeromaior = 0;
 for (index = 1; index<numbers.length; index += 1){
-  if(contador > numbers[index]){
+  if(numbers[index]%2!=0){
     numeromaior = contador;
     contador = contador;
   }
---
+++ bloco4-2-6.js
@@ -0,0 +1,14 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35];
+
+contador = 0;
+for (index = 0; index<numbers.length; index += 1){
+  if(numbers[index]%2!=0){
+    contador += 1;
+  }
+}
+if (contador == 0){
+  console.log('nenhum valor ímpar encontrado');
+}
+else {
+console.log(contador);
+}
