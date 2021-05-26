71b6274c317d280c794df05189cf4611918e0f4c
Julio Cesar <juliosinu@gmail.com>
2021-05-26 20:21:03 -0300

    exemplo 1 block 5.1 complete

---
+++ exempo1script.js
@@ -0,0 +1,10 @@
+document.getElementById("page-title").innerText = 'Senhor dos aneis';
+
+document.getElementById("second-paragraph").innerText = 'Candy shop - 50 Cent';
+
+document.getElementById("subtitle").innerText = 'Musica preferida';
+ 
+document.getElementsByClassName('.paragraph').style.fontSize = '50px';
+ 
+let el = document.getElementById("subtitle");
+el.style.color = 'red';
